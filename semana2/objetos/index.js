1db98751892257e36d278994e3f1d21f78cdb6c7
João Filippe <joaofilippe@outlook.com>
2021-06-21 15:30:40 -0300

    Adicionando o arquivo

---
+++ semana2/objetos/index.js
@@ -0,0 +1,11 @@
+const pessoa = {
+    nome: 'João Filippe', 
+    apelidos: ['Johnny', 'Jão', 'Tranks']
+ }
+ 
+const function apresentacao(pessoa) {
+    console.log
+}
+
+ // Exemplo de saída
+ "Eu sou Amanda, mas pode me chamar de: Amandinha, Mandinha ou Mandi"
