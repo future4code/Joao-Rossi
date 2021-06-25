4ea150459c3e35431a47bc1503bed056a18d4d24
João Filippe <joaofilippe@outlook.com>
2021-06-25 19:02:57 -0300

    Terminando o projeto

--- semana2/projeto/nossoJogo.js
+++ semana2/projeto/nossoJogo.js
@@ -1,123 +1,39 @@
-/* 
-
-REGRAS DO JOGO:
-
-As regras do jogo são as seguintes:
-
-- Existem 2 jogadores: o usuário e o computador.
-- Os jogadores usam um baralho com muitas cartas. As cartas têm um número (A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J , Q, K) 
-e um naipe (Copas (♥️), Paus (♣️), Ouros(♦️) e Espadas(♠️)). 
-Por exemplo, uma carta do baralho é o dois de Paus (2♣️). 
-Existem 52 possibilidades de cartas diferentes (13 números e 4 naipes).
-- Os jogadores compram cartas do baralho. Ao comprar uma carta do baralho, ela pode ser de qualquer uma das 52 cartas,
-com probabilidade igual.
-- As cartas tem um valor de acordo com seu número (a carta 4♣️ tem valor 4, e a carta 9♦️ tem valor 9).
-As cartas J, Q e K tem valor 10, e a carta A tem valor 11.
-- O jogo inicia com cada jogador (usuário e computador) recebendo 2 cartas.
-A pontuação de cada jogador é a soma do valor das suas cartas.
-- O jogador com a maior pontuação ganha a rodada.
-
-
-MONTANDO O JOGO:
-
-Essa etapa do projeto consiste em criar um programa que:
-
-1 - Imprime uma mensagem no console "Boas vindas ao jogo de Blackjack!".
-
-2 - Envia um `confirm`, perguntando ao usuário: "Quer iniciar uma nova rodada?".
-
-- 💡 Dica
-
-    `confirm` é um comando que cria uma caixa com dois botões: `ok` e `cancelar`, permitindo fazer perguntas de "sim" e "não". Caso o usuário responda sim (`ok`) o comando devolve o valor `true`, caso contrário, devolve `false`. Desta forma, podemos usar condicionais para decidir o que fazer em cada caso
-
-    ```jsx
-    if(confirm("pergunta de sim ou não")) {
-    	// o que fazer se o usuário clicar "ok"
-    } else {
-    	// o que fazer se o usuário clicar "cancelar"
-    }
-    ```
-
-3 - Se o usuário responder `cancel`, imprime uma mensagem no console "O jogo acabou".
-
-4 - Se o usuário responder `Ok`, o programa deve iniciar uma nova rodada.
-
-
-
-COMEÇANDO A RODADA:
-
-A rodada consiste em sortear 2 cartas para cada jogador, definir a pontuação de cada jogador e informar o vencedor (usuário ou computador), ou empate, caso as pontuações sejam iguais.
-
-5 - Nós preparamos um método que permite sortear uma carta. **Ele já está pronto e você só precisa invocá-lo**. Cada carta sorteada é um objeto com duas propriedades: um **texto** que representa o que é escrito na carta; e um **valor** que mostra a pontuação da carta. 
-
-- 💡 Dica
-
-    ```jsx
-    const carta = comprarCarta(); 
-    // Sorteia uma carta. Por exemplo, o rei de ouros
-
-    console.log(carta.texto) 
-    // imprime o texto da carta. Nesse caso: "K♦️"
-
-    console.log(carta.valor) 
-    // imprime o valor da carta (um número). Nesse caso: 10)
-    ```
-
-6 - O programa deve mostrar, no console, as cartas e pontuação de cada jogador no formato mostrado abaixo
-
-```jsx
-"Usuário - cartas: Q♣️ 10♣️  - pontuação 20" 
-"Computador - cartas: Q♣️ 10♣️  - pontuação 20"
-```
-
-7 - Além disso, deve indicar o vencedor ou um empate
-
-```jsx
-"Empate!"
-"O usuário ganhou!"
-"O computador ganhou!"
-```
-
-
-
-
-*/
-
-
-
-/**
- * EXEMPLO DE UTILIZAÇÃO DA 'comprarCarta'
- * 
- * 
-    const carta = comprarCarta(); // Sorteia uma carta. Por exemplo, o rei de ouros
-    
-    console.log(carta.texto) // imprime o texto da carta. Exemplo: "K♦️" (indica "K" de ouros)
-    console.log(carta.valor) // imprime o valor da carta (um número). Exemplo: 10 (dado que "K" vale 10)
- * 
- * 
- * 
- */
-
-
 // INÍCIO DO JOGO:
 
 console.log("Boas vindas ao jogo de Blackjack!");
 
-const  carta = comprarCarta()
-
 const inicioJogo = () => {
-   if (confirm('Quer iniciar uma nova rodada?')){
-    let cartaJogador = (comprarCarta())
-    let cartaComputador = (comprarCarta())
+  if (confirm("Quer iniciar uma nova rodada?")) {
+    // Distribuindo as cartas:
+    let carta1Jogador = comprarCarta();
+    let carta2Jogador = comprarCarta();
+    let carta1Computador = comprarCarta();
+    let carta2Computador = comprarCarta();
 
-    
+    // Somando os pontos:
+    let maoJogador = carta1Jogador.valor + carta2Jogador.valor;
+    let maoComputador = carta1Computador.valor + carta2Computador.valor;
+
+   //  Imprimindo as mensagens no console:
+    console.log(`As cartas tiradas pelo Jogador são: ${carta1Jogador.texto} e ${carta2Jogador.texto}.
+O total de pontos é: ${maoJogador}`);
+
+    console.log(`As cartas tiradas pelo Computador são: ${carta1Computador.texto} e ${carta2Computador.texto}.
+O total de pontos é: ${maoComputador}`);
+
+   // Contando os pontos e declarando o vencedor:
+    if (maoJogador > maoComputador) {
+      console.log(`O Jogador ganhou o Jogo!`);
+    } else if (maoJogador === maoComputador) {
+      console.log(`Tivemos um empate!`);
+    } else if (maoJogador < maoComputador) {
+      console.log(`O Computador ganhou!`);
+    }
+  } else {
+    console.log("O jogo acabou!");
+  }
+};
 
 
-
-
-   } else {
-      console.log('O jogo acabou!')
-   }
-}
-
-inicioJogo()
+// Chamando a função:
+inicioJogo();
