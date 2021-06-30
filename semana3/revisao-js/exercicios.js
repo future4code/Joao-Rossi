023f9e1a7000f9bc4cb8219c30e85e64256f6e12
João Filippe <joaofilippe@outlook.com>
2021-06-30 13:54:34 -0300

    Exercício 06

--- semana3/revisao-js/exercicios.js
+++ semana3/revisao-js/exercicios.js
@@ -68,6 +68,24 @@ function retornaQuantidadeElementos(array) {
 // EXERCÍCIO 06
 function retornaExpressoesBooleanas() {
 
+
+// const booleano1 = true
+// const booleano2 = false
+// const booleano3 = !booleano2 = true
+// const booleano4 = !booleano3 = false
+
+// a) `booleano1 && booleano2 && !booleano4` → false
+
+// b) `(booleano1 && booleano2) || !booleano3` → false
+
+// c)  `(booleano2 || booleano3) && (booleano4 || booleano1)` →  true
+
+// d) `!(booleano2 && booleano3) || !(booleano1 && booleano3)` → true
+
+// e) `!(booleano1) && !(booleano3) || (!booleano4 && booleano3 && booleano3)` → true
+
+return [false, false, true, true, true]
+
 }
 
 // EXERCÍCIO 07
