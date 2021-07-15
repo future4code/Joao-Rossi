d27f2344c23fd0167025b01dd451b2b6f4df9e88
João Filippe <joaofilippe@outlook.com>
2021-07-15 09:20:29 -0300

    feat/ adicionando os arquivos finais

--- semana5/labedin/src/components/CardGrande/CardGrande.js
+++ semana5/labedin/src/components/CardGrande/CardGrande.js
@@ -1,16 +1,41 @@
 import React from 'react';
-import './CardGrande.css'
+import styled from 'styled-components'
+
+const CardContainer = styled.div`
+  display: flex;
+  align-items: center;
+  border: 1px solid black;
+  padding: 20px 10px;
+  margin-bottom: 10px;
+  height: 200px;
+`;
+
+const CardImage = styled.img`
+  width: 70px;
+  margin-right: 10px;
+  border-radius: 50%;
+`;
+
+const BigText = styled.h4`
+  margin-bottom: 15px;
+`;
+
+const ContentContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-items: flex-start;
+`;
 
 function CardGrande(props) {
-    return (
-        <div className="bigcard-container">
-            <img src={ props.imagem } />
-            <div>
-                <h4>{ props.nome }</h4>
-                <p>{ props.descricao }</p>
-            </div>
-        </div>
-    )
+  return (
+    <CardContainer>
+      <CardImage src={props.imagem} />
+      <ContentContainer>
+        <BigText>{props.nome}</BigText>
+        <p>{props.descricao}</p>
+      </ContentContainer>
+    </CardContainer>
+  );
 }
 
 export default CardGrande;
