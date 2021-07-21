42d8978172e836b32b5b33ecb7372a82561f00eb
joaofilippe <joaofilippe@outlook.com>
2021-07-20 23:17:25 -0300

    Adicionando os arquivos

--- semana6/formulario/src/App.js
+++ semana6/formulario/src/App.js
@@ -1,24 +1,25 @@
-import logo from './logo.svg';
+
 import './App.css';
+import styled from 'styled-components';
+import DadosGerais from './components/Etapa1.js'
+
+const Cabecalho = styled.h1 `
+  display: flex;
+  justify-content: center;
+  font-family: Segoe UI;
+`
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Cabecalho>
+        Formulário
+      </Cabecalho>
+      <DadosGerais/>
     </div>
+
+    
   );
 }
 
