a3d2b0041139109101a710a4b3fe08d56691e03d
joaofilippe <joaofilippe@outlook.com>
2021-08-01 21:32:32 -0300

    feat/ adicionando o que eu consegui fazer

--- semana7/projeto/labefy/src/App.js
+++ semana7/projeto/labefy/src/App.js
@@ -1,28 +1,53 @@
-import React from 'react';
-import TelaCriarPlaylist from './components/CriarPlaylist';
+import React from "react";
+import styled from "styled-components";
+import PlaylistCreator from "./components/PlaylistCreator";
+import PlaylistManager from "./components/PlaylistManager"
+
+
+const Container = styled.body `
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  margin: 0;
+  padding: 0;
+`
+
+const ContainterPlaylist = styled.div `
+  display: flex;
+  flex-direction: column;
+  width: 400px;
+  height: 100vh;
+  background-color: #2c4d5e;
+`
+
+const ContainerManager = styled.div `
+  display: flex;
+`
+
+const ContainerMain = styled.div `
+  display: flex;
+`
 
 class App extends React.Component {
-    
-  state = {
-    telaAtual: "inicio"
-  }
-
-  telaCriarPlaylist = () => {
-    switch (this.state.telaAtual){
-      case "inicio":
-        return <div>Tela Inicial</div>
-      case "criar":
-        return <TelaCriarPlaylist />
-    }
-  }
   
-  render() {
-    return (
-      <div>
-      {this.telaCriarPlaylist()}
-      </div>
-    )
-  }
+  render () {
+
+  return (
+    <Container>
+      <ContainerMain>
+        <ContainterPlaylist>
+          <PlaylistCreator />
+          <PlaylistManager />
+        </ContainterPlaylist>
+        <ContainerManager>
+          Espaço que irá mudar
+          <p>Adicionar Músicas</p>
+          <p>Adicionar Playlist</p>
+        </ContainerManager>
+      </ContainerMain>
+    </Container>
+  );
+}
 }
 
 export default App;
