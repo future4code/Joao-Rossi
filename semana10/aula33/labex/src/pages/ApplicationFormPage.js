09b63dd1a48e0861fb5e6869f2bb8b91cb281ead
joaofilippe <joaofilippe@outlook.com>
2021-08-19 16:15:37 -0300

    iniciando o form

--- semana10/aula33/labex/src/pages/ApplicationFormPage.js
+++ semana10/aula33/labex/src/pages/ApplicationFormPage.js
@@ -1,8 +1,12 @@
-import React from 'react';
+import React from "react";
 
-
-export default function ApplicationFormPage () {
-
-    return <h1>ApplicationFormPage</h1>
-
+export default function ApplicationFormPage() {
+  return (
+    <div>
+      <h1>ApplicationFormPage</h1>
+      <form onSubmit={null}>
+          <input/>
+      </form>
+    </div>
+  );
 }
