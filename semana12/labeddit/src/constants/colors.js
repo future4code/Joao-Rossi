e878b4732b57cfee24726b9887f95ba9b1ab4596
joaofilippe <joaofilippe@outlook.com>
2021-08-31 11:15:46 -0300

    commitando para trocar de branch

---
+++ semana12/labeddit/src/constants/colors.js
@@ -0,0 +1,3 @@
+export const primaryColor = '#28639b';
+
+export const secondaryColor = '#9ac9f5'
--- semana12/labeddit/src/pages/WelcomePage/WelcomePage.js
+++ semana12/labeddit/src/pages/WelcomePage/WelcomePage.js
@@ -1,9 +1,13 @@
 import React from "react";
+import {Button} from "@material-ui/core"
 
 const WelcomePage = () => {
   return (
     <div>
       <h1>WelcomePage</h1>
+      <Button variant="contained" color='primary'>
+        Fazer Login
+      </Button>
     </div>
   );
 }
