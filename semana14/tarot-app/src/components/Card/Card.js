57a24639263de3ad6876594b1ced8dbb44764fb7
joaofilippe <joaofilippe@outlook.com>
2021-09-17 10:44:51 -0300

    feat/ adicionando as funções e dados que serão utilizados

--- semana14/tarot-app/src/components/Card/Card.js
+++ semana14/tarot-app/src/components/Card/Card.js
@@ -1,5 +1,23 @@
 import React from 'react';
 
+// Requerimento de dados:
+const data = require("../../assets/tarot.json");
+
+const baseImagesUrl = data.imagesUrl;
+
+const imageBackCard = data.imageBackCard;
+
+const cards = data.cards;
+
+
+// Funções que serão usadas:
+
+const getRandomInt = (min, max) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+};
+
 
 export default function Card(){
 
