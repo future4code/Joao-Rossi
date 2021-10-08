ff5595f4745655180646a41b6dd87602b4a776de
joaofilippe <joaofilippe@outlook.com>
2021-10-08 12:18:12 -0300

    feat/ terminando algumas adições

---
+++ semana17/revisao/src/endpoints/getActorByName.ts
@@ -0,0 +1,13 @@
+import { Response, Request } from "express";
+import sele
+
+export default async function getActorByName (
+    req: Request,
+    res: Response
+) {
+    try {
+        const actor = await selectActorByName 
+    } catch (error:any | undefined) {
+        
+    }
+}
