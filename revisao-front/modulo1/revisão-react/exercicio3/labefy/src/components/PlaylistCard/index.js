290fa156012d8b0678c3f6282471df9448e9457d
joaofilippe <joaofilippe@outlook.com>
2022-02-07 19:25:59 -0300

    adicionando mais mudanças

--- revisao-front/modulo1/revisão-react/exercicio3/labefy/src/components/PlaylistCard/index.js
+++ revisao-front/modulo1/revisão-react/exercicio3/labefy/src/components/PlaylistCard/index.js
@@ -2,20 +2,36 @@ import styled from 'styled-components';
 
 const CardContainer = styled.div`
     display: flex;
+    background-color: rgb(150, 150, 200);
+    width: 8.725em;
+    height: 8.725em;
+    border-radius: 0.85em;
     flex-direction: column;
     align-items: center;
-    font-size: 36pt;
-    padding: 1em;
-    font-weight: 10px;
+    justify-content: space-between;
+    padding: 1.725em;
+    margin: 0.5em;
 `;
 
-const Button = styled.button `
+const Text = styled.div`
+    text-align: center;
+    font-size: 20pt;
+    font-weight: 1200;
+`;
+
+const Button = styled.button`
     display: flex;
+    background-color: rgba(0, 0, 0, 0);
     width: 20px;
     justify-content: center;
     font-weight: 900;
-`
+`;
 
 export default function PlaylistCard(props) {
-    return <CardContainer>{props.name}<Button> X </Button></CardContainer>;
+    return (
+        <CardContainer>
+            <Text>{props.name}</Text>
+            <Button> ❌ </Button>
+        </CardContainer>
+    );
 }
