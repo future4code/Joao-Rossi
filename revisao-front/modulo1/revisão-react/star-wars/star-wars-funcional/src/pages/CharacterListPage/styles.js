49c30a30727a739c74ec1941fa35254f1b922074
joaofilippe <joaofilippe@outlook.com>
2022-02-08 15:44:02 -0300

    Update styles.js

--- revisao-front/modulo1/revisão-react/star-wars/star-wars-funcional/src/pages/CharacterListPage/styles.js
+++ revisao-front/modulo1/revisão-react/star-wars/star-wars-funcional/src/pages/CharacterListPage/styles.js
@@ -1,9 +1,9 @@
 import styled from 'styled-components';
 
-export const CharacterListContainer = styled.div `
-display: flex;
-padding: 2em;
-`
+export const CharacterListContainer = styled.div`
+    display: flex;
+    padding: 2em;
+`;
 
 export const CharacterListText = styled.h3`
     font-family: 'Segoe UI', Tahoma, Geneva, Verdana, sans-serif;
@@ -11,7 +11,12 @@ export const CharacterListText = styled.h3`
 `;
 
 export const CharacterCard = styled.div`
+    border-radius: 5px;
     border-style: double;
     border-color: rgb(100, 80, 120);
+    padding: 0%.5em;
     margin: 0.5em;
+    &:hover{
+        background-color: rgb(180,180,180);
+    }
 `;
